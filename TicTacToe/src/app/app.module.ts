c15e8d72fe032a46fe243ef7ac6b260a2272f2fd
unknown <tpatelia01@gmail.com>
2022-11-06 15:28:52 +0200

    first commit

---
+++ TicTacToe/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { BoardComponent } from './board/board.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GameComponent,
+    BoardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
